2699398480f6dbc8a0d83afb0d43e1b49ad5dfb5
Koala <joscha.leon.haberstock@gmail.com>
2018-03-15 09:54:15 +0100

    v.1.0

---
+++ Data/Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour {
+
+    public Transform playerBody;
+    public float mouseSensitivity;
+
+    float xAxisClamp = 0.0f;
+
+    [SerializeField] Vector3 targetRotCam;
+    [SerializeField] float rotAmountY;
+    [SerializeField] float rotAmountX;
+
+    private void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update ()
+    {
+        RotateCamera();
+	}
+
+    void RotateCamera()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        rotAmountX = mouseX * mouseSensitivity;
+        rotAmountY = mouseY * mouseSensitivity;
+
+        xAxisClamp -= rotAmountY;
+
+        targetRotCam = transform.rotation.eulerAngles;
+        Vector3 targetRotBody = playerBody.rotation.eulerAngles;
+
+        targetRotCam.x -= rotAmountY;
+        targetRotCam.z = 0;
+        targetRotCam.y += rotAmountX;
+
+        if(xAxisClamp > 90)
+        {
+            xAxisClamp = 90;
+            targetRotCam.x = 90;
+        }
+        else if(xAxisClamp < -90)
+        {
+            xAxisClamp = -90;
+            targetRotCam.x = 270;
+        }
+
+        transform.rotation = Quaternion.Euler(targetRotCam);
+        playerBody.rotation = Quaternion.Euler(targetRotBody);
+
+    }
+
+}
---
+++ Data/Assets/Scripts/Raycast.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycast : MonoBehaviour {
+
+	
+	
+	// Update is called once per frame
+	void Update () {
+
+        RaycastHit hit;
+        float Distance;
+
+        Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
+        Debug.DrawRay(transform.position, forward, Color.green);
+
+        if (Physics.Raycast(transform.position, (forward), out hit)) {
+            Distance = hit.distance;
+            print (Distance + " " + hit.collider.gameObject.name);
+        }
+	}
+}
